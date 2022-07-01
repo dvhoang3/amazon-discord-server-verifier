e7c93f21b859e79ce59b51ea330b5a0c932b54af
dvhoang3 <44732960+dvhoang3@users.noreply.github.com>
2022-07-01 15:53:41 -0500

    create discord bot to verify users for amazon disc

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES",
+    "GUILD_MEMBERS"
+  ]
+})
+
+let bot = {
+  client,
+  prefix: "!",
+  owners: ["238409444038410241"]
+}
+
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
+client.buttons = new Discord.Collection()
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot
+
+client.login(process.env.TOKEN)
